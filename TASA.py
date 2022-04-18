c8f4d3c8d56dd071fe0e94909f02e3bb73d4fd86
Pashase <61194472+Pashase@users.noreply.github.com>
2022-04-18 15:00:33 +0300

    Fixed TASA architecture
    Made embeddings decoding by LSTM decoder layer work correct

--- TASA.py
+++ TASA.py
@@ -5,7 +5,7 @@ from vocab import Vocab
 
 
 class TASA(nn.Module):
-    def __init__(self, inp_voc, emb_size=64, hid_size=128, seq_len=100):
+    def __init__(self, inp_voc, emb_size=100, hid_size=100, seq_len=100):
         """
         Complex LSTM model
         """
@@ -13,7 +13,7 @@ class TASA(nn.Module):
 
         self.inp_voc = inp_voc
         self.hid_size = hid_size
-
+        
         self.emb_inp = nn.Embedding(len(inp_voc), emb_size)
         self.enc0 = nn.LSTM(emb_size, hid_size, batch_first=True)
 
@@ -21,59 +21,53 @@ class TASA(nn.Module):
         self.logits = nn.Linear(hid_size, len(inp_voc))
         self.linear_influence = nn.Linear(seq_len, seq_len)
         self.sigmoid = nn.Sigmoid()
-
+        
     def forward(self, sessions, taus):
         """ Apply model in training mode """
-        inp_emb = self.emb_inp(sessions)
-        deltas = self.sigmoid(self.linear_influence(taus))
-        v_hats = inp_emb * deltas.unsqueeze(-1)
-        last_state = self.encode(v_hats)
+        last_state = self.encode(sessions, taus)
 
         return self.decode(last_state, sessions, taus)
 
-    def encode(self, inp, **flags):
+
+    def encode(self, sessions, taus, **flags):
         """
-        Takes symbolic input sequence, computes initial state
+        Takes an input sequence, computes initial state
         :param inp: matrix of input tokens [batch, time]
         :returns: initial decoder state tensors, one or many
-        """
-        batch_size = inp.shape[0]
-        output, last_state = self.enc0(inp)
+        """        
+        inp_emb = self.emb_inp(sessions)
+        deltas = self.sigmoid(self.linear_influence(taus))
+        v_hats = inp_emb * deltas.unsqueeze(-1)
+        
+        output, last_state = self.enc0(v_hats)
 
-        return [last_state]
+        return last_state
 
-    def decode_step(self, prev_state, prev_v_hats, **flags):
+    def decode_step(self, prev_lstm0_state, prev_v_hats, **flags):
         """
         Takes previous decoder state and tokens, returns new state and logits for next tokens
-        :param prev_state: a list of previous decoder state tensors, same as returned by encode(...)
-        :param prev_v_hats: previous output tokens, an int vector of [batch_size]
-        :return: a list of next decoder state tensors, a tensor of logits [batch, len(out_voc)]
         """
-        prev_lstm0_state = prev_state[0]
-        prev_lstm0_state = (prev_lstm0_state[0].squeeze(0), prev_lstm0_state[1].squeeze(0))
-
-        new_lstm0_state = self.dec0(prev_v_hats, prev_lstm0_state)
-        new_dec_state = [new_lstm0_state]
-        output_logits = self.logits(new_lstm0_state[0])
+        
+        h, c = self.dec0(prev_v_hats, prev_lstm0_state)
+        new_dec_state = (h, c)
+        output_logits = self.logits(h)
 
         return new_dec_state, output_logits
 
     def decode(self, initial_state, sessions, taus, **flags):
         """ Iterate over reference tokens (out_tokens) with decode_step """
-
-        batch_size = sessions.shape[0]
-        state = initial_state
-
-        logits_sequence = []
+        
+        state = (initial_state[0].squeeze(0), initial_state[1].squeeze(0))
+        
         S, T = sessions.clone(), taus.clone()
         S[:0] = inp_voc.bos_ix
         T[:0] = inp_voc.start_tau
-
         with torch.no_grad():
             inp_emb = self.emb_inp(S)
             deltas = self.sigmoid(self.linear_influence(T))
             v_hats = inp_emb * deltas.unsqueeze(-1)
 
+        logits_sequence = []
         for i in range(sessions.shape[1]):
             state, logits = self.decode_step(state, v_hats[:, i])
             logits_sequence.append(logits)
@@ -82,10 +76,10 @@ class TASA(nn.Module):
 
     def decode_inference(self, initial_state, max_len=100, **flags):
         """ Generate translations from model (greedy version) """
-
+        
         batch_size, device = len(initial_state[0]), initial_state[0].device
         state = initial_state
-        outputs = [torch.full([batch_size], self.out_voc.bos_ix, dtype=torch.int64,
+        outputs = [torch.full([batch_size], self.out_voc.bos_ix, dtype=torch.int64, 
                               device=device)]
         all_states = [initial_state]
 
@@ -93,5 +87,12 @@ class TASA(nn.Module):
             state, logits = self.decode_step(state, outputs[-1])
             outputs.append(logits.argmax(dim=-1))
             all_states.append(state)
-
+        
         return torch.stack(outputs, dim=1), all_states
+
+    def translate_lines(self, inp_lines, **kwargs):
+        inp = inp_voc.to_matrix(inp_lines).to(device)
+        initial_state = self.encode(inp, **kwargs)
+        out_ids, states = self.decode_inference(initial_state, **kwargs)
+
+        return out_voc.to_lines(out_ids.cpu().numpy()), states
